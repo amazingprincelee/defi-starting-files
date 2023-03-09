e5e528d37ee7d68c98a07d79908bb0dceb38516f
princelee <princeleepraise@gmail.com>
2023-03-09 09:32:43 +0100

    token contract creation stage 1

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+require('babel-polyfill');
+require('babel-register');
+
+
+
+module.exports = {
+    networks: {
+        development:{
+            host:'127.0.0.1',
+            port: '7545',
+            network_id: '*'
+        },
+    },
+    
+    contracts_directory:'./src/contracts',
+    contracts_build_directory: './src/truffle_abis',
+    compilers: {
+        solc: {
+            version: '^0.8.19',
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+        }
+    }
+}
